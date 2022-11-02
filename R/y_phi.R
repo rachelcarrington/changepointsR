c3f65648e23790c8cbcf3f0ca0d63d66f6c0685d
Rachel Carrington <r.j.carrington@lancaster.ac.uk>
2022-11-02 22:35:23 +0000

    Update y_phi.R

--- R/y_phi.R
+++ R/y_phi.R
@@ -2,18 +2,18 @@
 #'
 #' @description ...
 #'
-#' @param y Numeric data vector
-#' @param nu Numeric data vector of same length as y
-#' @param phi Scalar
-#' @param nu2 Scalar; squared 2-norm of nu
-#' @param nuTy Scalar; value of nu^T y
+#' @param y Numeric data vector.
+#' @param nu Numeric vector of same length as \code{y}.
+#' @param phi Numeric.
+#' @param nu2 Squared 2-norm of \code{nu}.
+#' @param nuTy Numeric; value of \code{t(nu)^T %*% y}.
 #'
 #' @return Numeric vector
 #' @export
 #'
 #' @examples
-#' y <- rnorm(20) + c(rep(1,10), rep(-1,10))
-#' nu <- c(rep(0,5), rep(1/5,5), rep(-1/5,5), rep(0,5))
+#' y <- rnorm(20) + c(rep(1, 10), rep(-1, 10))
+#' nu <- c(rep(0, 5), rep(1/5, 5), rep(-1/5, 5), rep(0, 5))
 #' phi <- 2
 #' y_phi(y, nu, phi)
 #'
@@ -23,7 +23,7 @@ y_phi <- function( y, nu, phi, nu2=NULL, nuTy=NULL ){
     nu2 <- sum(nu^2)
   }
   if ( is.null(nuTy) ){
-    nuTy <- as.numeric( t(nu) %*% y )
+    nuTy <- as.numeric(t(nu) %*% y)
   }
   y2 <- y + nu2^(-1) * (phi - nuTy) * nu
 
