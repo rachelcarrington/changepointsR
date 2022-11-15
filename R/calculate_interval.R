641baf4ad6eb4cd7144b4318060663604513cbfd
Rachel Carrington <r.j.carrington@lancaster.ac.uk>
2022-11-15 17:37:59 +0000

    Update calculate_interval.R

--- R/calculate_interval.R
+++ R/calculate_interval.R
@@ -11,7 +11,6 @@
 #' @param nuTy value of nu^T y
 #' @param threshold minimum changepoint threshold used in binary segmentation algorithm
 #' @param n.cp max. number of changepoints to detect of binary segmentation algorithm
-#' @param cs ?
 #'
 #' @return A 2-dimensional vector
 #' @export
@@ -25,7 +24,7 @@ calculate_interval <- function(y, method, results, nu, nu2=NULL, nuTy=NULL, n.cp
 
     b <- results$results$b[ results$results$cp==1 ]
     d <- results$results$d[ results$results$cp==1 ]
-    interval <- calculate_interval_bs(y, nu, b, d, nu2=nu2, nuTy=nuTy, n.cp=n.cp)
+    interval <- calculate_interval_bs(y, nu, b, d, threshold=results$threshold, nu2=nu2, nuTy=nuTy, n.cp=n.cp)
 
   } else if ( method=="wbs" ){
     
