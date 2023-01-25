45ecccebd5d3d3f364afe2ca9d56c51412df3533
Rachel Carrington <r.j.carrington@lancaster.ac.uk>
2023-01-25 11:51:48 +0000

    Update calculate_pvals.R

--- R/calculate_pvals.R
+++ R/calculate_pvals.R
@@ -12,9 +12,11 @@
 #' @param threshold Minimum threshold for changepoint detection. Ignored if \code{results} is specified.
 #' @param maxiter Positive integer. Maximum number of changepoints to find in changepoint algorithm. Ignored if
 #' \code{results} is specified
-#' @param h Positive integer (>=2) or \code{NULL}. Window size. If \code{NULL} then the changepoints either side of the
-#' changepoint of interest are used to define the window.
-#' @param cp_bound Logical. ...
+#' @param h Positive integer (>=2) or vector of length 2 or \code{NULL}. Window size. If \code{NULL} then the changepoints either side of 
+#' the changepoint of interest are used to define the window.
+#' @param gamma Number in (0, 1]. If \code{h = NULL}, then ...
+#' @param cp_bound Logical. If \code{TRUE}, then if there is an estimated changepoint within the window that fix under the null hypothesis,
+#' this changepoint will be used as the boundary of the window.
 #' @param sigma2 Variance of \code{y}.
 #' @param eps0 Hyperparameter for calculating S.
 #' @param include_original Logical. Whether to include the \eqn{\psi} value corresponding to the observed data in place of
@@ -36,10 +38,13 @@
 #' @details
 #' Given a changepoint of interest \eqn{\tau_j}, there are two options for the null hypothesis:
 #' \itemize{
-#' \item There are no changepoints within a window size \eqn{h} of \eqn{\tau_j}. In this case \code{h} should be supplied.
-#' If \code{cp_bound} is \code{TRUE}, then if there is a another estimated changepoint within \code{h} of \eqn{\tau_j}, this will be used.
-#' \item There are no other changepoints within ...
-#' \item There are no other changepoints between \eqn{\tau_{j-1}} and \eqn{\tau_{j+1}}. In this case \code{h} should be set to \code{NULL}.
+#' \item There are no changepoints within a window size \eqn{h} of \eqn{\tau_j}. In this case \code{h} should be supplied. It is also possible
+#' to use different values of \code{h} on either side of \eqn{\tau_j}, in which case \code{h} should be a vector of length 2.
+#' If \code{cp_bound = TRUE}, then if there is another estimated changepoin within the \code{h} of \eqn{\tau_j} then the window size used
+#' will be the minimum of \code{h} and the distance between \eqn{\tau_j} and the closest estimated changepoint to \eqn{\tau_j}.
+#' \item There are no other changepoints between \eqn{\tau_{j-1}} and \eqn{\tau_{j+1}}. In this case \code{h} should be set to \code{NULL}
+#' and \code{gamma} should be 1. (This is the default for the function.)
+#' \item ...
 #' }
 #'
 #' @return A list.
@@ -63,7 +68,7 @@
 #' results <- binary_segmentation(y, threshold=4)
 #' calculate_pvals(y, method="bs", results=results, h=10)
 #'
-calculate_pvals <- function(y, method="bs", results=NULL, N=10, threshold=NULL, maxiter=NULL, h=NULL, cp_bound=TRUE, sigma2=1, eps0=0.01,
+calculate_pvals <- function(y, method="bs", results=NULL, N=10, threshold=NULL, maxiter=NULL, h=NULL, gamma=1, cp_bound=TRUE, sigma2=1, eps0=0.01,
                             include_original=TRUE, num_pvals=NULL, random_samples=NULL, num_rand_samples=NULL, seeded=FALSE,
                             decay=NULL, return_probs=FALSE){
 
@@ -101,12 +106,6 @@ calculate_pvals <- function(y, method="bs", results=NULL, N=10, threshold=NULL, 
     num_pvals <- length(b)
   }
 
-  Z <- diag(2*h) - matrix( 1/(2*h), nrow=2*h, ncol=2*h ) - h/2 * crossprod(t(c(rep(1/h, h), rep(-1/h, h))))
-  Z[ abs(Z) < 10^(-10) ] <- 0
-  U <- svd(Z)$u[,1:(2*h-2)]
-  U[ abs(U) < 10^(-10) ] <- 0
-
-
   p_value <- rep(NA, num_pvals)
   P_both <- P_phi_in_S <- matrix(0, nrow=num_pvals, ncol=N)
 
@@ -114,45 +113,73 @@ calculate_pvals <- function(y, method="bs", results=NULL, N=10, threshold=NULL, 
   for ( jj in 1:num_pvals ){
 
     ## Calculate nu
-    if ( is.null(h) ){
+    if ( is.null(h) ){ # use changepoint on either side to determine h
       cps <- c(0, sort(b, decreasing=FALSE), n)
-      j <- (1:length(cps))[cps==b[1]]
+      j <- (1:length(cps))[cps==b[jj]]
       nu <- rep(0, n)
       nu[ (cps[j-1]+1):cps[j] ] <- 1/(cps[j] - cps[j-1])
       nu[ (cps[j]+1):cps[j+1] ] <- -1/(cps[j+1] - cps[j])
       nu2 <- 1/(cps[j] - cps[j-1]) + 1/(cps[j+1] - cps[j])
+      h1 <- cps[j] - cps[j-1]
+      h2 <- cps[j+1] - cps[j]
+    } else if ( length(h) >= 2 ) {
+      if ( b[jj] - h[1] < 0 ){
+        nu <- c( rep(1/b[jj], b[jj]), rep(-1/h[2], h[2]), rep(0, n - b[jj] - h[2]) )
+        nu2 <- 1/b[jj] + 1/h[2]
+        h1 <- b[jj]
+        h2 <- h[2]
+      } else if ( b[jj] + h[2] > n ){
+        nu <- c( rep(0, b[jj] - h[1]), rep(1/h[1], h[1]), rep(-1/(n - b[jj]), n - b[jj]) )
+        nu2 <- 1/h[1] + 1/(n - b[jj])
+        h1 <- h[1]
+        h2 <- n - b[jj]
+      } else {
+        nu <- c( rep(0, b[jj] - h[1]), rep(1/h[1], h[1]), rep(-1/h[2], h[2]), rep(0, n - b[jj] - h[2]) )
+        nu2 <- 1/h[1] + 1/h[2]
+        h1 <- h[1]
+        h2 <- h[2]
+      }
     } else {
       stopifnot( h >= 2 )
-      if ( b[jj] - h < 0 ){
-        nu <- c( rep(1/b[jj], b[jj]), rep(-1/h, h), rep(0, n - b[jj] - h) )
-        nu2 <- 1/b[jj] + 1/h
-      } else if ( b[jj] + h > n ){
-        nu <- c( rep(0, b[jj] - h), rep(1/h, h), rep(-1/(n - b[jj]), n - b[jj]) )
-        nu2 <- 1/h + 1/(n - b[jj])
+      cps <- c(0, sort(b[-jj]), n)
+      if ( cps %in% (b[jj] - h + 1):b[jj] ){
+        h1 <- b[jj] - max(cps[cps %in% (b[jj] - h + 1):b[jj]])
       } else {
-        nu <- c( rep(0, b[jj] - h), rep(1/h, h), rep(-1/h, h), rep(0, n - b[jj] - h) )
-        nu2 <- 2/h
+        h1 <- h
       }
+      if ( cps %in% (b[jj] + 1):(b[jj] + h) ){
+        h2 <- max(cps[cps %in% (b[jj] + 1):(b[jj] + h)]) - b[jj]
+      } else {
+        h2 <- h
+      }
+      nu <- c(rep(0, b[jj] - h1), rep(1/h1, h1), rep(-1/h2, h2), rep(0, n - b[jj] - h2))
+      nu2 <- 1/h1 + 1/h2
     }
+    nh <- h1 + h2
 
     nuTy <- as.numeric(t(nu) %*% y)
 
     #### Construct Y s.t. y_t = Y_t^T (1,phi,Psi)
-    Y <- cbind(y, matrix(0, nrow=n, ncol=2*h-1))
-    Y[(b[jj] - h + 1):(b[jj] + h), 1] <- mean(y[(b[jj] - h + 1):(b[jj] + h)]) ## replace elements in (b-h, b+h) with mean
-    Y[(b[jj] - h + 1):(b[jj] + h), 2] <- 1/sum(nu^2)* nu[(b[jj] - h + 1):(b[jj] + h)] ## constant of phi
-    Y[(b[jj] - h + 1):(b[jj] + h), 3:ncol(Y)] <- U ## constants of Psi
-    colnames(Y) <- c("y0", "phi", paste0("psi", 1:(2*h-2)))
+    Z <- diag(nh) - matrix( 1/(nh), nrow=nh, ncol=nh ) - 1/nu2 * crossprod(t(c(rep(1/h1, h1), rep(-1/h2, h2))))
+    Z[ abs(Z) < 10^(-10) ] <- 0
+    U <- svd(Z)$u[,1:(nh-2)]
+    U[ abs(U) < 10^(-10) ] <- 0
 
+    Y <- cbind(y, matrix(0, nrow=n, ncol=nh-1))
+    Y[nu!=0, 1] <- mean(y[(nu!=0)]) ## replace elements in (b-h, b+h) with mean
+    Y[nu!=0, 2] <- 1/nu2 * nu[(nu!=0)] ## constant of phi
+    Y[nu!=0, 3:ncol(Y)] <- U ## constants of Psi
+    colnames(Y) <- c("y0", "phi", paste0("psi", 1:(nh-2)))
+    
     ## Generate psi values
     if ( include_original ){
       if ( N > 1 ){
-        Psi <- rbind( matrix(rnorm((2*h - 2) * (N - 1), sd=sqrt(sigma2)), nrow=(N - 1)), as.numeric(t(U) %*% y[(b[jj]-h+1):(b[jj]+h)]) )
+        Psi <- rbind( matrix(rnorm((nh - 2) * (N - 1), sd=sqrt(sigma2)), nrow=(N - 1)), as.numeric(t(U) %*% y[(b[jj]-h1+1):(b[jj]+h2)]) )
       } else {
-        Psi <- matrix(as.numeric(t(U) %*% y[(b[jj]-h+1):(b[jj]+h)]), nrow=1)
+        Psi <- matrix(as.numeric(t(U) %*% y[(b[jj]-h1+1):(b[jj]+h2)]), nrow=1)
       }
     } else {
-      Psi <- matrix( rnorm((2*h-2)*N, sd=sqrt(sigma2)), nrow=N )
+      Psi <- matrix(rnorm((nh-2)*N, sd=sqrt(sigma2)), nrow=N)
     }
 
 
@@ -160,13 +187,13 @@ calculate_pvals <- function(y, method="bs", results=NULL, N=10, threshold=NULL, 
 
       ### Calculate S
       y_new <- Y[,1] + nuTy / nu2 * nu ## this is necessary b/c when calculating S it assumes we need to subtract it
-      y_new[(b[jj]-h+1):(b[jj]+h)] <- y_new[(b[jj]-h+1):(b[jj]+h)] + U %*% t(Psi[iter,,drop=FALSE])
+      y_new[(b[jj]-h1+1):(b[jj]+h2)] <- y_new[(b[jj]-h1+1):(b[jj]+h2)] + U %*% t(Psi[iter,,drop=FALSE])
 
-      r2 <- find_cps( y_new, method=method, threshold=threshold, maxiter=maxiter, num_rand_ints=num_rand_samples, rand_ints=random_samples, seeded=seeded, decay=decay )
+      r2 <- find_cps(y_new, method=method, threshold=threshold, maxiter=maxiter, num_rand_ints=num_rand_samples, rand_ints=random_samples, seeded=seeded, decay=decay)
       b2 <- r2$results$b[ r2$result$cp==1 ]
       b2 <- b2[!is.na(b2)]
-      if ( length( b2 ) >= 1 ){
-        if ( r2$results$b[1] == b[jj] ){
+      if ( length(b2) >= 1 ){
+        if ( !is.null(h) & (r2$results$b[1] == b[jj]) ){
           S <- calculate_S(y_new, results=r2, nu=nu, threshold=threshold, maxiter=maxiter, method=method, first_cp_only=TRUE, nuTy=nuTy, rand_ints=random_samples, seeded=seeded, decay=decay)
           ## nuTy should be calculated using the original y!
         } else {
@@ -179,15 +206,41 @@ calculate_pvals <- function(y, method="bs", results=NULL, N=10, threshold=NULL, 
 
       ### Calculate probability
 
-      ### Take intervals which are for the correct values of b
-      max_cps_found <- (ncol(S) - 2)/2
-      S2 <- S[ S[,3]==b[jj], ]
-      if ( max_cps_found > 1 ){
-        for ( j in 2:max_cps_found ){
-          S2 <- rbind(S2, S[ S[,j+2]==b[jj], ])
+      if ( is.null(h) ){
+
+        ### Find which intervals contain the correct combination of changepoints
+        ###### First get rid of intervals which contain too many changepoints
+        max_cps_found <- (ncol(S) - 2)/2
+        if ( max_cps_found > length(results$changepoints) ){
+          S2 <- S[ is.na(S[,(2+length(results$changepoints)+1)]), ]
+        } else {
+          S2 <- S
         }
+        ###### Get rid of intervals which contain too few changepoints
+        S2 <- S2[ !is.na(S2[,(2+length(results$changepoints))]), ]
+        ###### Check we have the correct combination of changepoints
+        z <- rep(0, nrow(S2))
+        cps <- sort(results$changepoints)
+        for ( j in 1:nrow(S2) ){
+          if ( sum(abs(sort(S2[j, 3:(length(cps) + 2)]) - cps)) < 10^(-10) ){
+            z[j] <- 1
+          }
+        }
+        S2 <- S2[z==1,]
+
+      } else {
+
+        ### Find intervals which contain b[jj]
+        max_cps_found <- (ncol(S) - 2)/2
+        S2 <- S[ S[,3]==b[jj], ]
+        if ( max_cps_found > 1 ){
+          for ( j in 2:max_cps_found ){
+            S2 <- rbind(S2, S[ S[,j+2]==b[jj], ])
+          }
+        }
+        S2 <- S2[ !is.na(S2$b1), ]
+
       }
-      S2 <- S2[ !is.na(S2$b1), ]
 
       ### Calculate P(phi \in S)
       P_phi_in_S[jj, iter] <- sum( pnorm(S2[,2] / sqrt(nu2 * sigma2)) - pnorm(S2[,1] / sqrt(nu2 * sigma2)) )
@@ -217,8 +270,6 @@ calculate_pvals <- function(y, method="bs", results=NULL, N=10, threshold=NULL, 
     P_phi_in_S <- NA
   }
 
-  return( list(p_value=p_value, P_both=P_both, P_phi_in_S=P_phi_in_S) )
+  return( list(p_value=p_value, P_both=P_both, P_phi_in_S=P_phi_in_S, S=S2) )
 
 }
-
-
