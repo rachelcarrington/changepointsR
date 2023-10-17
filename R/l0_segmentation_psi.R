f7a26df90e55f37a140a53bf58c0acbc61e86d1a
Rachel Carrington <r.j.carrington@lancaster.ac.uk>
2023-10-17 22:22:37 +0100

    Update l0_segmentation_psi.R

--- R/l0_segmentation_psi.R
+++ R/l0_segmentation_psi.R
@@ -40,135 +40,146 @@ l0_segmentation_psi <- function(y, lambda, N, h, sigma2=1, sig=4, include_origin
 
   fit <- ChangepointInference::changepoint_estimates(y, "L0", lambda)
   b <- fit$change_pts
-  if ( is.null( num_pvals ) ){
+  if ( is.null(num_pvals) ){
     num_pvals <- length(fit$change_pts)
   }
 
-  if ( include_original ){
-    ### Original p-values
-    P_both_orig <- P_phi_in_S_orig <- rep(0, num_pvals)
-    pvals_orig <- rep(NA, num_pvals)
-    z <- ChangepointInference::changepoint_inference(y, "L0-fixed", tuning_parameter=lambda, window_size=h, sig=sig, return_conditioning_set=TRUE)
+  if ( length(b) >= 1 ){
 
-    for ( jj in 1:num_pvals ){
-      nu <- c(rep(0,b[jj]-h), rep(1/h,h), rep(-1/h,h), rep(0,n-b[jj]-h))
-      nu2 <- 2/h
-      nuTy <- as.numeric(t(nu)%*%y)
+    if ( include_original ){
+      # Original p-values
+      P_both_orig <- P_phi_in_S_orig <- rep(0, num_pvals)
+      pvals_orig <- rep(NA, num_pvals)
+      z <- ChangepointInference::changepoint_inference(y, "L0-fixed", tuning_parameter=lambda, window_size=h, sig=sig, return_conditioning_set=TRUE)
 
-      S <- z$conditioning_sets[[jj]]
-      S2 <- S[ S$contained==1, ]
-
-      ### Calculate P(phi \in S)
-      P_phi_in_S_orig[jj] <- sum( pnorm( S2$max_mean / sqrt(nu2*sigma2) ) - pnorm( S2$min_mean / sqrt(nu2*sigma2) ) )
-
-      ### Calculate P(phi > |nuTy| & phi \in S)
-      for ( i in 1:nrow(S2) ){
-        if ( S2$max_mean[i] > abs(nuTy) ){
-          P_both_orig[jj] <- P_both_orig[jj] +
-            pnorm( S2$max_mean[i] / sqrt(nu2*sigma2) ) - pnorm( max( c( abs(nuTy), S2$min_mean[i] ) ) / sqrt(nu2*sigma2) )
-        }
-        if ( S2$min_mean[i] < (-1)*abs(nuTy) ){
-          P_both_orig[jj] <- P_both_orig[jj] +
-            pnorm( min( c( -abs(nuTy), S2$max_mean[i] ) ) / sqrt(nu2*sigma2) ) - pnorm( S2$min_mean[i] / sqrt(nu2*sigma2) )
-        }
-      }
-      pvals_orig[jj] <- P_both_orig[jj] / P_phi_in_S_orig[jj]
-    }
-
-    N2 <- N - 1
-
-  } else {
-
-    pvals_orig <- P_both_orig <- P_phi_in_S_orig <- NA
-    N2 <- N
-
-  }
-
-
-  ### New p-values
-
-  if ( length(b) >= 1 & N2 >= 1 ){
-
-    alpha <- rep(1, 2*h)
-    alpha2 <- 2*h
-    Z <- diag(2*h) - 1/alpha2 * crossprod(t(alpha)) - 1/(2/h) * crossprod(t(c(rep(1/h,h), rep(-1/h,h))))
-    U <- svd(Z)$u[,1:(2*h-2)]
-
-    if ( is.null(num_pvals) ){
-      num_pvals <- length(b)
-    } else {
-      num_pvals <- min( c(num_pvals, length(b)) )
-    }
-
-    p_val <- rep(NA, num_pvals)
-    P_both <- P_phi_in_S <- matrix(NA, nrow=num_pvals, ncol=N2)
-
-    for ( j in 1:num_pvals ){
-
-      if ( b[j] >= h & b[j] <= (n-h-1) ){
-
-        nu <- c(rep(0,b[j]-h), rep(1/h,h), rep(-1/h,h), rep(0,n-b[j]-h))
+      for ( jj in 1:num_pvals ){
+        nu <- c(rep(0, b[jj] - h), rep(1/h, h), rep(-1/h, h), rep(0, n - b[jj] - h))
         nu2 <- 2/h
         nuTy <- as.numeric(t(nu)%*%y)
 
-        Psi <- matrix( rnorm( N2*(2*h-2), sd=sqrt(sigma2) ), nrow=N2 )
-        for ( iter in 1:N2 ){
-          y_new <- y
-          y_new[(b[j]-h+1):(b[j]+h)] <- y_new[(b[j]-h+1):(b[j]+h)] + U %*% Psi[iter,]
+        S <- z$conditioning_sets[[jj]]
+        S2 <- S[ S$contained==1, ]
 
-          fit_new <- ChangepointInference::changepoint_estimates(y_new, "L0", lambda)
-          b_new <- fit_new$change_pts
+        # Calculate P(phi \in S)
+        P_phi_in_S_orig[jj] <- sum( pnorm( S2$max_mean / sqrt(nu2*sigma2) ) - pnorm( S2$min_mean / sqrt(nu2*sigma2) ) )
 
-          ### I think this works (we need to make sure that b[j] %in% b_new)
-          phi <- 10
-          while ( !( b[j] %in% b_new ) ){
-            y_new <- y_phi(y_new, nu, phi)
-            phi <- phi + 10
-            b_new <- ChangepointInference::changepoint_estimates(y_new, "L0", lambda)$change_pts
+        # Calculate P(phi > |nuTy| & phi \in S)
+        for ( i in 1:nrow(S2) ){
+          if ( S2$max_mean[i] > abs(nuTy) ){
+            P_both_orig[jj] <- P_both_orig[jj] +
+              pnorm( S2$max_mean[i] / sqrt(nu2*sigma2) ) - pnorm( max( c( abs(nuTy), S2$min_mean[i] ) ) / sqrt(nu2*sigma2) )
           }
-
-          z <- ChangepointInference::changepoint_inference(y_new, 'L0-fixed', lambda, window_size = h, sig = sig, return_conditioning_sets = TRUE)
-
-          ### Make sure we select the right changepoint!
-          S <- z$conditioning_sets[[ (1:length(z$change_pts))[ z$change_pts==b[j] ] ]]
-
-          ### Keep rows of S for which b[j] %in% b ("contained" tells us whether b_new[1] %in% b)
-          S2 <- S[ S$contained==1, ]
-
-          ### Calculate P(phi \in S)
-          P_phi_in_S[j,iter] <- sum( pnorm( S2$max_mean / sqrt(nu2*sigma2) ) - pnorm( S2$min_mean / sqrt(nu2*sigma2) ) )
-
-          ### Calculate P(phi > |nuTy| & phi \in S)
-          P_both[j,iter] <- 0
-          for ( i in 1:nrow(S2) ){
-            if ( S2$max_mean[i] > abs(nuTy) ){
-              P_both[j,iter] <- P_both[j,iter] +
-                pnorm( S2$max_mean[i] / sqrt(nu2*sigma2) ) - pnorm( max( c( abs(nuTy), S2$min_mean[i] ) ) / sqrt(nu2*sigma2) )
-            }
-            if ( S2$min_mean[i] < (-1)*abs(nuTy) ){
-              P_both[j,iter] <- P_both[j,iter] +
-                pnorm( min( c( -abs(nuTy), S2$max_mean[i] ) ) / sqrt(nu2*sigma2) ) - pnorm( S2$min_mean[i] / sqrt(nu2*sigma2) )
-            }
-
+          if ( S2$min_mean[i] < (-1)*abs(nuTy) ){
+            P_both_orig[jj] <- P_both_orig[jj] +
+              pnorm( min( c( -abs(nuTy), S2$max_mean[i] ) ) / sqrt(nu2*sigma2) ) - pnorm( S2$min_mean[i] / sqrt(nu2*sigma2) )
           }
-
         }
-
-        ### Calculate p-value estimates
-        if ( include_original ){
-          p_val[j] <- ( sum(P_both[j,]) + P_both_orig[j] ) / ( sum(P_phi_in_S[j,]) + P_phi_in_S_orig[j] )
-        } else {
-          p_val[j] <- sum( P_both[j,] ) / sum( P_phi_in_S[j,] )
-        }
-
+        pvals_orig[jj] <- P_both_orig[jj] / P_phi_in_S_orig[jj]
       }
+
+      N2 <- N - 1
+
+    } else {
+
+      pvals_orig <- P_both_orig <- P_phi_in_S_orig <- NA
+      N2 <- N
 
     }
 
 
+    # New p-values
+
+    if ( N2 >= 1 ){
+
+      alpha <- rep(1, 2*h)
+      alpha2 <- 2*h
+      Z <- diag(2*h) - 1/alpha2 * crossprod(t(alpha)) - 1/(2/h) * crossprod(t(c(rep(1/h, h), rep(-1/h, h))))
+      U <- svd(Z)$u[,1:(2*h - 2)]
+
+      if ( is.null(num_pvals) ){
+        num_pvals <- length(b)
+      } else {
+        num_pvals <- min(c(num_pvals, length(b)))
+      }
+
+      p_val <- rep(NA, num_pvals)
+      P_both <- P_phi_in_S <- matrix(NA, nrow=num_pvals, ncol=N2)
+
+      for ( j in 1:num_pvals ){
+
+        if ( b[j] >= h & b[j] <= n - h - 1 ){
+
+          nu <- c(rep(0, b[j] - h), rep(1/h, h), rep(-1/h, h), rep(0,n - b[j] - h))
+          nu2 <- 2/h
+          nuTy <- as.numeric(t(nu) %*% y)
+
+          Psi <- matrix(rnorm(N2 * (2 * h - 2), sd=sqrt(sigma2)), nrow=N2)
+          for ( iter in 1:N2 ){
+            y_new <- y
+            y_new[(b[j] - h + 1):(b[j] + h)] <- y_new[(b[j] - h + 1):(b[j] + h)] + U %*% Psi[iter,]
+
+            fit_new <- ChangepointInference::changepoint_estimates(y_new, "L0", lambda)
+            b_new <- fit_new$change_pts
+
+            # I think this works (we need to make sure that b[j] %in% b_new)
+            phi <- 10
+            while ( !(b[j] %in% b_new) ){
+              y_new <- y_phi(y_new, nu, phi)
+              phi <- phi + 10
+              b_new <- ChangepointInference::changepoint_estimates(y_new, "L0", lambda)$change_pts
+            }
+
+            z <- ChangepointInference::changepoint_inference(y_new, 'L0-fixed', lambda, window_size = h, sig = sig, return_conditioning_sets = TRUE)
+
+            # Make sure we select the right changepoint!
+            S <- z$conditioning_sets[[(1:length(z$change_pts))[z$change_pts==b[j]]]]
+
+            # Keep rows of S for which b[j] %in% b ("contained" tells us whether b_new[1] %in% b)
+            S2 <- S[S$contained == 1,]
+
+            # Calculate P(phi \in S)
+            P_phi_in_S[j,iter] <- sum( pnorm( S2$max_mean / sqrt(nu2*sigma2) ) - pnorm( S2$min_mean / sqrt(nu2*sigma2) ) )
+
+            # Calculate P(phi > |nuTy| & phi \in S)
+            P_both[j,iter] <- 0
+            for ( i in 1:nrow(S2) ){
+              if ( S2$max_mean[i] > abs(nuTy) ){
+                P_both[j,iter] <- P_both[j,iter] +
+                  pnorm(S2$max_mean[i] / sqrt(nu2 * sigma2)) - pnorm(max(c(abs(nuTy), S2$min_mean[i])) / sqrt(nu2 * sigma2))
+              }
+              if ( S2$min_mean[i] < (-1)*abs(nuTy) ){
+                P_both[j,iter] <- P_both[j,iter] +
+                  pnorm(min(c(-abs(nuTy), S2$max_mean[i])) / sqrt(nu2 * sigma2)) - pnorm(S2$min_mean[i] / sqrt(nu2 * sigma2))
+              }
+
+            }
+
+          }
+
+          # Calculate p-value estimates
+          if ( include_original ){
+            p_val[j] <- (sum(P_both[j,]) + P_both_orig[j]) / (sum(P_phi_in_S[j,]) + P_phi_in_S_orig[j])
+          } else {
+            p_val[j] <- sum(P_both[j,]) / sum(P_phi_in_S[j,])
+          }
+
+        }
+
+      }
+
+    } else {
+
+      P_both <- P_both_orig
+      P_phi_in_S <- P_phi_in_S_orig
+      p_val <- pvals_orig
+
+    }
+
+
+
   } else {
 
-    p_val <- numeric(0)
+    p_val <- pvals_orig <- P_both <- P_phi_in_S <- P_phi_in_S_orig <- P_both_orig <- nuTy <- NA
     print("No changepoints detected.")
 
   }
