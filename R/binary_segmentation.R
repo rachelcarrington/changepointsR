85ac63a53ab9429ca3130bf55c02ca1a4f7ff6df
Rachel Carrington <r.j.carrington@lancaster.ac.uk>
2023-10-17 22:23:24 +0100

    Update binary_segmentation.R

--- R/binary_segmentation.R
+++ R/binary_segmentation.R
@@ -33,72 +33,77 @@ binary_segmentation <- function( y, threshold=NULL, maxiter=NULL ){
 
   n <- length(y)
 
-  ## Set threshold, if unspecified
+  # Set threshold, if unspecified
   if ( is.null(threshold) ){
     threshold <- sd(y) * sqrt(2 * log(n))
   } else if ( threshold < 0 ){
     threshold <- 0
   }
 
-  ## Set maximum number of iterations (defaults to n - 1, if unspecified)
+  # Set maximum number of iterations (defaults to n - 1, if unspecified)
   if ( is.null(maxiter) ){
     maxiter <- n - 1
   } else if ( maxiter < 1 || maxiter > n - 1 ){
     stop("maxiter should be between 1 and length(y) - 1")
   }
 
-  ## Find first changepoint
+  results <- matrix(NA, nrow=maxiter, ncol=7)
+  colnames(results) <- c("iter", "s", "e", "b", "d", "lrs", "cp")
+
+  # Find first changepoint
   iter <- 1
   s <- 1
   e <- n
-  cusum_stats <- cusum(y)
-  b <- which.max( abs(cusum_stats) ) ## changepoint candidate
-  lrs <- max( abs(cusum_stats) ) ## CUSUM statistic at this point
-  d <- ifelse( cusum_stats[b] > 0, -1, 1 ) ## direction of change
-  cp <- ( lrs > threshold ) ## = 1 if CUSUM statistic is above threshold, 0 otherwise
-  results <- data.frame(iter, s, e, b, d, lrs, cp)
+  C0 <- cumsum(y)
+  cusum_stats <- cusum(y, cumsums=C0)
+  b <- which.max(abs(cusum_stats)) # changepoint candidate
+  lrs <- max(abs(cusum_stats)) # CUSUM statistic at this point
+  d <- ifelse(cusum_stats[b] > 0, -1, 1) # direction of change
+  cp <- lrs > threshold # = 1 if CUSUM statistic is above threshold, 0 otherwise
+#  results <- data.frame(iter, s, e, b, d, lrs, cp)
+  results[iter,] <- c(iter, s, e, b, d, lrs, cp)
 
-  if ( cp & maxiter > 1 ){
+  # Split dataset at changepoints and repeat
+  while ( iter < maxiter ) {
+    
+    if ( cp ){
+      # If a CP was detected last time
+      iter <- iter + 1
+      
+      # Take results for which the changepoint threshold was reached
+      cps <- sort(c(results[,"b"]))
+      s <- c(1, cps + 1)
+      e <- c(cps, n)
 
-    ## Split dataset at changepoints and repeat
-    while ( iter < maxiter ) {
-
-      if ( results$cp[length(results$cp)]==1 ){
-
-        ### If a CP was detected last time
-        
-        iter <- iter + 1
-
-        ### Take results for which the changepoint threshold was reached
-        cps <- sort( c(results$b[ results$cp==1 ]) )
-        s <- c(1, cps + 1)
-        e <- c(cps, n)
-
-        cusum_stats <- rep(0, n - 1)
-        for ( k in 1:length(s) ){
-          if ( e[k] > s[k] ){
-            cusum_stats[ s[k]:(e[k] - 1) ] <- cusum(y[s[k]:e[k]])
-          }
+      cusum_stats <- rep(0, n - 1)
+      for ( k in 1:length(s) ){
+        if ( e[k] > s[k] ){ # changed this from s[k]
+          cusum_stats[s[k]:(e[k] - 1)] <- cusum(y, s=s[k], e=e[k], cumsums=C0)
         }
-
-        b <- which.max( abs(cusum_stats) ) ## changepoint candidate
-        lrs <- max( abs(cusum_stats) ) ## CUSUM statistic at this point
-        d <- ifelse( cusum_stats[b] > 0, -1, 1 ) ## direction of change
-        cp <- ( lrs > threshold ) ## = 1 if CUSUM statistic is above threshold, 0 otherwise
-        results <- rbind( results, c(iter, max(s[s <= b]), min(e[e > b]), b, d, lrs, cp) )
-
-      } else {
-
-        ### If no CP was detected, end while loop
-        iter <- maxiter
-
       }
+
+      b <- which.max(abs(cusum_stats)) # changepoint candidate
+      lrs <- max(abs(cusum_stats)) # CUSUM statistic at this point
+      d <- ifelse(cusum_stats[b] > 0, -1, 1) # direction of change
+      cp <- lrs > threshold # = 1 if CUSUM statistic is above threshold, 0 otherwise
+#      results <- rbind( results, c(iter, max(s[s <= b]), min(e[e > b]), b, d, lrs, cp) )
+      results[iter,] <- c(iter, max(s[s <= b]), min(e[e > b]), b, d, lrs, cp)
+
+    } else {
+
+      # If no CP was detected, end while loop
+      iter <- maxiter
+      results <- results[!is.na(rowSums(results)),,drop=FALSE]
 
     }
 
   }
 
-  changepoints <- results$b[ results$cp==1 ]
+  # Get changepoints
+  changepoints <- results[,"b"][results[,"cp"] == 1]
+
+  # Convert results to data frame
+  results <- data.frame(results)
 
   return( list( results=results, changepoints=changepoints, threshold=threshold, maxiter=maxiter ) )
 
