a2186f94791b9a9b0e6530dd7568dca6dc9c6d61
Rachel Carrington <68341047+rachelc245@users.noreply.github.com>
2024-01-15 15:55:03 +0000

    Update generate_random_intervals.R

--- R/generate_random_intervals.R
+++ R/generate_random_intervals.R
@@ -23,36 +23,36 @@ generate_random_intervals <- function( n, N, min_width=2 ){
   stopifnot( is.numeric(min_width) || is.integer(min_width) )
   stopifnot( min_width >= 0, min_width <= n )
 
-  ### Maximum number of random intervals for given n:
-  N_max <- (n - min_width + 1)*(n - min_width + 2)/2
+  # Maximum number of random intervals for given n:
+  N_max <- (n - min_width + 1) * (n - min_width + 2) / 2
 
   if ( N < N_max ){
 
-    rand_ints <- matrix( sample(1:n, 2*N, replace=TRUE), ncol=2 )
+    rand_ints <- matrix(sample(1:n, 2*N, replace=TRUE), ncol=2)
 
-    ### Delete and replace duplicates
+    # Delete and replace duplicates
     rand_ints <- t( apply(rand_ints, 1, sort) )
-    rand_ints <- rand_ints[ !(duplicated(rand_ints)), ]
+    rand_ints <- rand_ints[!(duplicated(rand_ints)), ]
     while( nrow(rand_ints) < N ){
-      rand_ints_2 <- matrix( sample(1:n, 2*(N - nrow(rand_ints)), replace=TRUE), ncol=2 )
-      rand_ints_2 <- t( apply(rand_ints_2, 1, sort) )
+      rand_ints_2 <- matrix(sample(1:n, 2 * (N - nrow(rand_ints)), replace=TRUE), ncol=2)
+      rand_ints_2 <- t(apply(rand_ints_2, 1, sort))
       rand_ints <- rbind(rand_ints, rand_ints_2)
-      rand_ints <- rand_ints[ !(duplicated(rand_ints)), ]
+      rand_ints <- rand_ints[!(duplicated(rand_ints)), ]
     }
 
-    ### Delete and replace any intervals which are smaller than min_width
+    # Delete and replace any intervals which are smaller than min_width
     for ( i in 1:N ){
       if ( abs(rand_ints[i,1] - rand_ints[i,2]) < (min_width - 1) ){
         if ( rand_ints[i,1] <= (min_width - 1) ){
-          rand_ints[i,2] <- sample( (rand_ints[i,1] + min_width - 1):n, 1 )
+          rand_ints[i,2] <- sample((rand_ints[i,1] + min_width - 1):n, 1)
         } else if ( rand_ints[i,1] >= (n - min_width + 1) ){
-          rand_ints[i,2] <- sample( 1:(rand_ints[i,1] - min_width + 1), 1 )
+          rand_ints[i,2] <- sample(1:(rand_ints[i,1] - min_width + 1), 1)
         } else {
-          rand_ints[i,2] <- sample( c(1:(rand_ints[i,1] - min_width + 1), (rand_ints[i,1] + min_width - 1):n), 1 )
+          rand_ints[i,2] <- sample(c(1:(rand_ints[i,1] - min_width + 1), (rand_ints[i,1] + min_width - 1):n), 1)
         }
       }
 
-      ### Order so that s is in column 1, e in column 2
+      # Order so that s is in column 1, e in column 2
       if ( rand_ints[i,1] > rand_ints[i,2] ){
         rand_ints[i,] <- rand_ints[i,2:1]
       }
@@ -60,11 +60,10 @@ generate_random_intervals <- function( n, N, min_width=2 ){
 
   } else {
 
-    ### Just use all possible intervals
+    # Just use all possible intervals
 
     ri2 <- matrix(rep(min_width:n, n - min_width + 1), nrow=n - min_width + 1)
-    rand_ints <- cbind( rep(1:(n - min_width + 1), (n - min_width + 1):1),
-                         as.vector(ri2[ lower.tri(ri2, diag=TRUE) ]) )
+    rand_ints <- cbind(rep(1:(n - min_width + 1), (n - min_width + 1):1), as.vector(ri2[lower.tri(ri2, diag=TRUE)]))
 
   }
 
