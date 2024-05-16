7113722c101e311d0fb7362b22da84602d003dc6
Carrington <carrinrj@lancaster.ac.uk>
2024-05-16 12:05:35 +0100

    Update cusum_phi_vec.R

--- R/cusum_phi_vec.R
+++ R/cusum_phi_vec.R
@@ -1,40 +1,40 @@
-#' Calculate vector of CUSUM statistics for data y, in terms of phi
+#' Calculate vector of CUSUM statistics for data x, in terms of phi
 #'
 #' @description Used inside \code{calculate_pvals}.
 #' 
-#' @param y Numeric vector of data.
+#' @param x Numeric vector of data.
 #' @param nu Numeric vector.
 #' @param nu2 Numeric; value of the squared 2-norm of \code{nu}.
-#' @param nuTy Value of nu^T y.
+#' @param nuTx Value of nu^T x.
 #' @param s Starting point for calculating CUSUM statistics, defaults to 1.
-#' @param e Ending point for calculating CUSUM statistics, degaults to \code{length(y)}.
+#' @param e Ending point for calculating CUSUM statistics, defaults to \code{length(x)}.
 #'
-#' @return Returns \code{length(y)} x 2 matrix such that 
-#' \code{cusum_phi_vec(y, nu)^T %*% c(1, phi) = cusum(y_phi(y, nu, phi))}.
+#' @return Returns \code{length(x)} x 2 matrix such that 
+#' \code{cusum_phi_vec(x, nu)^T %*% c(1, phi) = cusum(x_phi(x, nu, phi))}.
 #'
 #' @export
 #'
 #' @examples
-#' set.seed(100)
-#' y <- rnorm(100) + c(rep(1,40), rep(-1,60))
-#' results <- binary_segmentation(y)
+#' x <- rnorm(100) + c(rep(1,40), rep(-1,60))
+#' n <- length(x)
+#' results <- binary_segmentation(x)
 #' b <- results$changepoints[1]
-#' nu <- c(rep(1/b, b), rep(-1/(100-b), 100-b))
-#' cusum_phi_vec(y, nu)
+#' nu <- c(rep(1/b, b), rep(-1/(n - b), n - b))
+#' cusum_phi_vec(x, nu)
 #'
-cusum_phi_vec <- function( y, nu, nu2=NULL, nuTy=NULL, s=1, e=length(y), cumsums=NULL, nu_cumsums=NULL ){
+cusum_phi_vec <- function( x, nu, nu2=NULL, nuTy=NULL, s=1, e=length(x), cumsums=NULL, nu_cumsums=NULL ){
 
   if ( is.null(nu2) ){
     nu2 <- sum(nu^2)
   }
 
-  if ( is.null(nuTy) ){
-    nuTy <- as.numeric(t(nu) %*% y)
+  if ( is.null(nuTx) ){
+    nuTx <- as.numeric(t(nu) %*% x)
   }
 
   cusum_nu <- cusum(nu, s, e, cumsums=nu_cumsums)
-  cst <- cusum(y, s, e, cumsums=cumsums) - nuTy/nu2 * cusum_nu
-  coef <- 1/nu2 * cusum_nu
+  cst <- cusum(x, s, e, cumsums=cumsums) - nuTx/nu2 * cusum_nu
+  coef <- cusum_nu / nu2
 
   return( cbind(cst, coef) )
 
